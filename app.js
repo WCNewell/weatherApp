1ce6b927eb834b06b6257ab1a242decfd793775f
W. Clark Newell <clark@clarknewell.tech>
2019-04-04 20:34:19 -0600

    working on getting weather api

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ weatherApp.config(function ($routeProvider, $locationProvider) {
 
 weatherApp.service('cityService', function() {
 
-    this.city = "San Jose, CA"
+    this.city = ''
 
 })
 
@@ -34,13 +34,37 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function ($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$sce', 'cityService', function ($scope, $sce, cityService) {
 
     $scope.city = cityService.city;
+
+    $scope.appId = '1269643c3a92e4b5f9d6bd575054d50d'
+
+    $scope.getWeather = function() {
+        url = "http://api.openweathermap.org/data/2.5/weather?q="+$scope.city+"&mode=json&APPID="+$scope.appId;
+        $http.get(url).then(function(response) {
+            console.log(response.data)
+        }, function myError(response) {
+            console.log(response.statusText)
+        })
+    };
+
+    // $scope.weatherAPI = $sce.trustAsResourceUrl('https://api.openweathermap.org/data/2.5/weather',
+    //     { callback: "JSON_CALLBACK"},
+    //     { get: { method: "JSONP" }});
+
+    // $scope.weatherResult = $scope.weatherAPI.get({
+    //     q: $scope.city,
+    //     APPID: '1269643c3a92e4b5f9d6bd575054d50d'
+    // })
+
+    // console.log($scope.weatherResult);
 
 }]);
 
 
+
+// http: //api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1269643c3a92e4b5f9d6bd575054d50d
 // http: //api.openweathermap.org/data/2.5/forecast?id=524901&APPID=1269643c3a92e4b5f9d6bd575054d50d
 
 
